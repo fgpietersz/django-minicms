9341e9ca1166f57efeafe8d918e6fc4ed1dd659a
Graeme Pietersz <4478189+fgpietersz@users.noreply.github.com>
2015-11-23 12:59:56 +0530

    Added development media urls to project.
    Bugfixed and changed breadcrumbs to ancestors.

--- minicms_dev/urls.py
+++ minicms_dev/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+
 import minicms
 
 urlpatterns = [
     url(r'^pages/', include('minicms.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    ]
