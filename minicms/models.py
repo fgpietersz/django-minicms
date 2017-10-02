574f292aaa539c11e98c1c68e89fd33bbf13bdf9
Graeme Pietersz <graeme@pietersz.net>
2017-10-02 01:11:09 +0100

    Page is not shown if any ancestor is unpublished.

--- minicms/models.py
+++ minicms/models.py
@@ -15,7 +15,7 @@ class Page(models.Model):
     published = models.BooleanField(default=True)
     position = models.PositiveIntegerField(default=1000)
     parent = models.ForeignKey('self', related_name='children',
-        null=True, blank=True, db_index=True)
+                               null=True, blank=True, db_index=True)
     urlpath = models.CharField(max_length=1000, editable=False, unique=True)
 
     _siblings = None
@@ -23,11 +23,11 @@ class Page(models.Model):
     _child_pages = None
 
     class Meta:
-        unique_together = ('slug','parent')
+        unique_together = ('slug', 'parent')
         ordering = ('position',)
 
     def __str__(self):
-        return self.urlpath + '  ' + self.title
+        return self.urlpath + ': ' + self.title
 
     def get_absolute_url(self):
         return reverse('page', args=[self.urlpath])
@@ -50,7 +50,7 @@ class Page(models.Model):
         self._child_pages = None
 
     def siblings(self):
-        if self._siblings is not None:
+        if self._siblings is None:
             self._siblings = Page.objects.filter(
                 parent=self.parent, published=True)
         return self._siblings
@@ -59,16 +59,18 @@ class Page(models.Model):
         self.children.filter(published=True)
 
     def ancestors(self):
-        if self.parent is None:
-            return []
+        """Get ancestors"""
         if self._ancestors is not None:
             return self._ancestors
-        path_list = None
-        for i in self.urlpath.split('/')[:-1]:
-            if path_list:
-                path_list.append('/'.join((path_list[-1], i)))
-            else:
-                path_list = [i]
-        self._ancestors = Page.objects.filter(
-                urlpath__in=path_list, published=True).order_by('urlpath')
+        if self.parent is None:
+            self._ancestors = []
+        else:
+            path_list = None
+            for i in self.urlpath.split('/')[:-1]:
+                if path_list:
+                    path_list.append('/'.join((path_list[-1], i)))
+                else:
+                    path_list = [i]
+            self._ancestors = Page.objects.filter(
+                urlpath__in=path_list).order_by('urlpath')
         return self._ancestors
